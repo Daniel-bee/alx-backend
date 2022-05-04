8693511f93cf971fc72fb2e3f9b50ceea2c5b0c2
Daniel-bee <daniel123_86@live.com>
2022-05-04 19:15:28 +0300

    LFU cache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,35 +1,77 @@
-#!/usr/bin/env python3
-""" Python caching systems """
-
-from base_caching import BaseCaching
+#!/usr/bin/python3
+""" 4. MRU Caching
+"""
+from datetime import datetime
+from collections import defaultdict
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ LFU caching system """
-
+    """ BaseCache defines:
+      - overwrite functions 'put' and 'get' for implement
+      MRU caching system
+    """
     def __init__(self):
-        ''' Initialize class instance. '''
+        """ Initiliaze
+        """
         super().__init__()
-        self.current_keys = []
+        self.cache_by_time = {}
+        self.cache_by_frequency_use = defaultdict(int)  # >>> Default value: 0
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is not None or item is not None:
+        """
+        Assign to the dictionary self.cache_data the item
+        value for the key key
+        If the number of items in self.cache_data is higher
+        that BaseCaching.MAX_ITEMS:
+        - must discard the most recently used item (MRU algorithm)
+        - must print DISCARD: with the key discarded and following by
+        a new line
+        """
+        if key and item:
+            self.cache_by_time[key] = datetime.now()
             self.cache_data[key] = item
-            if key not in self.current_keys:
-                self.current_keys.append(key)
-            else:
-                self.current_keys.append(self.current_keys.pop(
-                    self.current_keys.index(key)))
-            if len(self.current_keys) > BaseCaching.MAX_ITEMS:
-                discarded_key = self.current_keys.pop(0)
-                del self.cache_data[discarded_key]
-                print('DISCARD: {}'.format(discarded_key))
+            self.cache_by_frequency_use[key] += 1
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # Sorted elements by frequency_used
+                frequency_use_filtered = {}
+                for k, v in self.cache_by_frequency_use.items():
+                    if k != key:
+                        frequency_use_filtered[k] = v
+                keys_by_frequency_used = sorted(frequency_use_filtered,
+                                                key=frequency_use_filtered.get)
+                key_to_delete = keys_by_frequency_used[0]
+
+                # There are more elements with same frequency used count?
+                count = frequency_use_filtered[key_to_delete]
+                posibles_elements_to_discard_dict = {}
+                for k, v in frequency_use_filtered.items():
+                    if v == count:
+                        posibles_elements_to_discard_dict[k] = v
+                if len(posibles_elements_to_discard_dict) > 1:
+                    elements_to_discard_by_time = {}
+                    for k, v in self.cache_by_time.items():
+                        if k in posibles_elements_to_discard_dict.keys():
+                            elements_to_discard_by_time[k] = v
+
+                    elements_by_time = sorted(
+                                          elements_to_discard_by_time,
+                                          key=elements_to_discard_by_time.get)
+                    key_to_delete = elements_by_time[0]
+
+                # Delete element with least_frequency_used
+                del self.cache_by_time[key_to_delete]
+                del self.cache_data[key_to_delete]
+                del self.cache_by_frequency_use[key_to_delete]
+                print('DISCARD: {}'.format(key_to_delete))
 
     def get(self, key):
-        """ Get an item by key """
-        if key is not None and key in self.cache_data:
-            self.current_keys.append(self.current_keys.pop(
-                self.current_keys.index(key)))
-            return self.cache_data.get(key)
-        return None
+        """
+            Return the value in self.cache_data linked to key
+        """
+        element = self.cache_data.get(key)
+        if element:
+            self.cache_by_time[key] = datetime.now()
+            self.cache_by_frequency_use[key] += 1
+        return element
